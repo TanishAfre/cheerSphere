1364ef2083dc17a01add14b8a9aa5f3ed730ee09
tanish <tanish.afre@gmail.com>
2023-11-19 19:23:36 +0000

    got button working

--- renderer.js
+++ renderer.js
@@ -6,13 +6,29 @@
  * to expose Node.js functionality from the main process.
  */
 
-const body = document.querySelector("body"),
-    sidebar = document.querySelector(".sidebar"),
-    toggle = document.querySelector(".sidebar-toggle"),
-    searchBtn = document.querySelector(".search-btn"),
-    modeSwitch = document.querySelector(".mode-switch"),
-    modeText = document.querySelector(".mode-text");
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
 
-    toggle.addEventListener("click", () => {
-        sidebar.classList.toggle("close");
-    });
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
+        
+    }
+});
