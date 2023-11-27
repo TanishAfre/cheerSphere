904bb90041d9b881b75077757025715d275cdf3d
Harsh Khatri <31386281+Ferreter@users.noreply.github.com>
2023-11-27 19:59:30 +0000

    Worked on minimize to tray functionality

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain, Tray } = require('electron')
 const { exec } = require('child_process'); //running python
 const path = require('path') //path
+const TrayWindow = require('electron-tray-window');
+
+let tray = null; // Tray instance
+let win = null; // BrowserWindow instance
 //I disabled developer mode but to enable it press Ctrl+Shift+I
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -19,6 +23,26 @@ const createWindow = () => {
    
   })
   win.loadFile('index.html')
+
+    // Create the Tray icon
+    tray = new Tray(path.join(__dirname, 'images/focus-mind.ico')); // Path to the tray icon
+    tray.setToolTip('Focus Mind'); // Tooltip for the tray icon
+    tray.on('click', () => {
+      // Restore the window when the tray icon is clicked
+      if (win.isVisible()) {
+        win.hide();
+      } else {
+        win.show();
+      }
+    });
+  
+    // Minimize the window to tray instead of closing
+  win.on('minimize', (event) => {
+    if (!app.isQuitting) {
+      event.preventDefault();
+      win.hide();
+    }
+  });
 
 
   ipcMain.on('minimize-window', () => {
@@ -41,11 +65,20 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow()
 
+  app.on('before-quit', () => {
+    app.isQuitting = true;
+  });
+  
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+  
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  }); 
-
+  });
   
 })
