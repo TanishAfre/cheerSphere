580ab0eea98460da97697896c29559bd6a7c9949
samolajide23 <d00231093@student.dkit.ie>
2023-12-05 14:38:22 +0000

    Added Close Focus Mode Button

--- chrome extension/Content.js
+++ chrome extension/Content.js
@@ -1,29 +1,38 @@
 const generateSTYLES = () => {
   return `<style>
-  #container {
-    position: fixed;
-    backdrop-filter: saturate(180%) blur(30px); 
-    height: 100vh;
-    width: 100vw;--
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: white;
-    display: flex;
-    z-index: 9999;
-  }
-
+    #container {
+      position: fixed;
+      backdrop-filter: saturate(180%) blur(30px);
+      height: 100vh;
+      width: 100vw;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      color: white;
+      z-index: 9999;
+    }
+    
     ._1 {
       font-size: 90px;
       font-weight: bold;
-      text-align: center;
     }
-
+    
     ._2 {
       font-size: 20px;
-      text-align: center;
-      margin-top: 100px;
-      position: absolute;
+      margin-top: 20px; /* Adjust as needed */
+    }
+    
+    #toggle {
+      margin-top: 20px; /* Adjust as needed */
+      padding: 10px;
+      border: none;
+      border-radius: 5px;
+      background-color: blue;
+      color: white;
+      cursor: pointer;
+      font-size: 20px;
+      font-weight: bold;
     }
   </style>`;
 };
@@ -33,35 +42,30 @@ const generateHTML = (pageName) => {
     <div id='container'>
         <div class='_1'>Focus Mode Enabled</div>
         <div class='_2'>Website Blocked: ${pageName}</div>
+        <button id='toggle'>Close Focus Mode</button>
     </div>`;
 };
+
 // Create a new element to hold content
 const overlayContainer = document.createElement("div");
 
 switch (window.location.hostname) {
   case "www.youtube.com":
-    overlayContainer.innerHTML = generateSTYLES() + generateHTML(window.location.hostname);
-    document.body.appendChild(overlayContainer);
-    break;
   case "www.facebook.com":
-    overlayContainer.innerHTML = generateSTYLES() + generateHTML(window.location.hostname);
-    document.body.appendChild(overlayContainer);
-    break;
   case "www.netflix.com":
-    overlayContainer.innerHTML = generateSTYLES() + generateHTML(window.location.hostname);
-    document.body.appendChild(overlayContainer);
-    break;
   case "www.roblox.com":
-    overlayContainer.innerHTML = generateSTYLES() + generateHTML(window.location.hostname);
-    document.body.appendChild(overlayContainer);
-    break;
   case "discord.com":
-    overlayContainer.innerHTML = generateSTYLES() + generateHTML(window.location.hostname);
-    document.body.appendChild(overlayContainer);
-    break;
   case "www.spotify.com":
     overlayContainer.innerHTML = generateSTYLES() + generateHTML(window.location.hostname);
     document.body.appendChild(overlayContainer);
+
+    // Add event listener to the Close Focus Mode button
+    const closeButton = document.getElementById('toggle');
+    if (closeButton) {
+      closeButton.addEventListener('click', () => {
+        // Remove the overlay container when the button is clicked
+        document.body.removeChild(overlayContainer);
+      });
+    }
     break;
 }
-//document.body.innerHTML = generateHTML(window.location.hostname);
