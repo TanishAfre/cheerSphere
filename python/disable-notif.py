afa12161a06fcc1f33620a6671be3b8251fc9f77
Harsh Khatri <31386281+Ferreter@users.noreply.github.com>
2024-02-04 15:48:46 +0000

    Worked on disabling notifications for backend

---
+++ python/disable-notif.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+
+# Simulate pressing the Windows key + N
+pyautogui.hotkey('win', 'n')
+# Wait a bit for the action to be recognized
+time.sleep(0.4)
+# Simulate pressing the Enter key
+pyautogui.press('enter')
+# Wait a bit before pressing the next key
+time.sleep(0.1)
+# Simulate pressing the Escape key
+pyautogui.press('esc')
