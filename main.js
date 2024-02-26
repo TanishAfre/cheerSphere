30bca1e5f686998a428646d191d4b9498daf71b2
Harsh Khatri <31386281+Ferreter@users.noreply.github.com>
2024-02-26 13:15:08 +0000

    :chart_with_upwards_trend: Added Readability in logs

--- main.js
+++ main.js
@@ -92,27 +92,31 @@ const createWindow = () => {
   // Event listener for the 'blackout' event
 
   ipcMain.on('close-blackout', (event, arg) => {
-    console.log('Blackout window closed');
-    appendToLog("Blackout window closed");
+    const timestamp = new Date().toLocaleString(); // Get current timestamp
+    console.log(timestamp +' - Blackout window closed');
+    appendToLog(timestamp +' - Blackout window closed');
     blackoutWin.close();
   });
 
   ipcMain.on('minimize-window', () => {
-    console.log('Minimizing window');
-    appendToLog("Minimizing window");
+    const timestamp = new Date().toLocaleString(); // Get current timestamp
+    console.log(timestamp +' - Minimizing window');
+    appendToLog(timestamp +' - Minimizing window');
     win.minimize();
   })
 
   ipcMain.on('close-window', () => {
-    console.log('Closing window');
-    appendToLog("Closing window");
+    const timestamp = new Date().toLocaleString(); // Get current timestamp
+    console.log( timestamp +' - Closing window');
+    appendToLog(timestamp +' - Closing window');
     win.close();
   })
 
   ipcMain.on('focus', () => {
 
     exec('python python/start-focus.py', (error, stdout, stderr) => {
-      const logMessage = `Error: ${error}, Stderr: ${stderr}, Stdout: ${stdout}`;
+      const timestamp = new Date().toLocaleString(); // Get current timestamp
+      const logMessage = `${timestamp} - Error: ${error}, Stderr: ${stderr}, Stdout: ${stdout}`;
     
       // Log to console
       console.log(logMessage);
@@ -123,9 +127,9 @@ const createWindow = () => {
   })
 
   ipcMain.on('notif', () => {
-
+    const timestamp = new Date().toLocaleString(); // Get current timestamp
     exec('python python/disable-notif.py', (error, stdout, stderr) => {
-      const logMessage = `Error: ${error}, Stderr: ${stderr}, Stdout: ${stdout}`;
+      const logMessage = `${timestamp} -Error: ${error}, Stderr: ${stderr}, Stdout: ${stdout}`;
     
       // Log to console
       console.log(logMessage);
@@ -136,8 +140,9 @@ const createWindow = () => {
   })
 
   ipcMain.on('blackout', (event, arg) => {
-    console.log('Creating blackout window');
-    appendToLog("Creating blackout window");
+    const timestamp = new Date().toLocaleString(); // Get current timestamp
+    console.log(timestamp +' - Creating blackout window');
+    appendToLog(timestamp +' - Creating blackout window');
     createBlackoutWindow();
   });
 
@@ -145,15 +150,16 @@ const createWindow = () => {
     const filePath = path.join(app.getPath('userData'), 'database/settings.json');
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
-
-        console.error('Error reading the file', err);
-        appendToLog("Error reading the file: " + err);
+        const timestamp = new Date().toLocaleString(); // Get current timestamp
+        console.error(timestamp +' - Error reading the file', err);
+        appendToLog(timestamp +' - Error reading the file: ' + err);
         event.reply('get-settings-file-response', 'error');
         return;
       }
+      const timestamp = new Date().toLocaleString(); // Get current timestamp
       event.reply('get-settings-file-response', data);
-      console.log('Settings file sent');
-      appendToLog("Settings file sent");
+      console.log(timestamp +' - Settings file sent');
+      appendToLog(timestamp +' -Settings file sent');
     });
   });
 
